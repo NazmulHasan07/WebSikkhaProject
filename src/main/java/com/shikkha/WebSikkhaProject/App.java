be3470792de7071a36573fc97a649c6bd00bef3c
Nazmul Hasan <nhrhandsome@gmail.com>
2018-01-07 17:24:45 -0500

    Updated project.

---
+++ src/main/java/com/shikkha/WebSikkhaProject/App.java
@@ -0,0 +1,13 @@
+package com.shikkha.WebSikkhaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
